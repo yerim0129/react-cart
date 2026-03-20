61796184ff3c2accc03866774d1ccc0e4fa15283
yeeerim <kom5327@naver.com>
2026-03-20 13:53:26 +0900

    feat : types 추가

--- ts-task-board/src/interfaces/ITask.ts
+++ ts-task-board/src/interfaces/ITask.ts
@@ -1,11 +1,13 @@
 import {IBaseEntity} from "./IBaseEntity";
 import {IUser} from "./IUser";
+import {TaskStatus} from "../types/task";
+import {TaskPriority} from "../types/task";
 
 export interface ITask extends IBaseEntity {
   title: string;
   description: string;
-  status: "TODO" | "IN_PROGRESS" | "DONE";
-  priority: "HIGH" | "MEDIUM" | "LOW";
+  status: TaskStatus;
+  priority: TaskPriority;
   assignee: IUser;
   dueDate: Date;
 }
---
+++ ts-task-board/src/types/task.ts
@@ -0,0 +1,2 @@
+export type TaskStatus = "TODO" | "IN_PROGRESS" | "DONE";
+export type TaskPriority = "HIGH" | "MEDIUM" | "LOW";
